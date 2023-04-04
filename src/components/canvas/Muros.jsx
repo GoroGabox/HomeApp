9c3431efdf993dc4abec5435d49b7e570c61cce9
GoroGabox <alfons.diaz97@gmail.com>
2023-04-04 03:22:26 -0300

    actulizacion modelo muros faltantes

--- src/components/canvas/Muros.jsx
+++ src/components/canvas/Muros.jsx
@@ -13,7 +13,7 @@ function Muros({ancho, longitud, altura}) {
 			>
 				<meshBasicMaterial color="red" wireframe={true}/>
 				<boxGeometry args={[1, 1, 1]} />
-				<Muro longitud={longitud} altura={altura} />
+				<MuroX longitud={longitud} altura={altura} />
 			</mesh>
 			<mesh 
 			position={[(ancho/2)-grosor/2, 0, 0]}
@@ -21,7 +21,7 @@ function Muros({ancho, longitud, altura}) {
 			>
 				<meshBasicMaterial color="blue" wireframe={true}/>
 				<boxGeometry args={[1, 1, 1]} />
-				<Muro longitud={longitud} altura={altura} />
+				<MuroX longitud={longitud} altura={altura} />
 			</mesh>
 			<mesh 
 			position={[0, 0, (longitud/2)-grosor/2]}
@@ -29,6 +29,7 @@ function Muros({ancho, longitud, altura}) {
 			>
 				<meshBasicMaterial color="orange" wireframe={true}/>
 				<boxGeometry args={[1, 1, 1]} />
+				<MuroY longitud={ancho} altura={altura} />
 			</mesh>
 			<mesh 
 			position={[0, 0, (-longitud/2)+grosor/2]}
@@ -36,6 +37,7 @@ function Muros({ancho, longitud, altura}) {
 			>
 				<meshBasicMaterial color="purple" wireframe={true}/>
 				<boxGeometry args={[1, 1, 1]} />
+				<MuroY longitud={ancho} altura={altura} />
 			</mesh>
 		</group>
 	)
@@ -43,7 +45,7 @@ function Muros({ancho, longitud, altura}) {
 
 export default Muros
 
-export function Muro({longitud, altura}) {
+export function MuroX({longitud, altura}) {
 	function mapLengthToRange(l, rangeMin, rangeMax) {
 		const lengthMin = 0;
 		const lengthMax = 100;
@@ -61,7 +63,7 @@ export function Muro({longitud, altura}) {
         for (let i = 0; i < numPilares; i++) {
 			const range = mapLengthToRange(length, -0.5, 0.5);
 			length += porcentajeEspacio
-            result.push(<Pilar ubicacion={range} altura={altura} key={i}/>);
+            result.push(<PilarX ubicacion={range} altura={altura} key={i}/>);
         }
         return result;
     }, [longitud, altura, numPilares]);
@@ -73,13 +75,58 @@ export function Muro({longitud, altura}) {
 	)
 }
 
-export function Pilar({ubicacion, altura}) {
+export function PilarX({ubicacion, altura}) {
 
 	return (
 		<>
 			<mesh
 			position={[ 0, 0,ubicacion]}
 			scale={[1,1,0.01]}
+			>
+				<boxGeometry args={[1, 1, 1]}/>
+				<meshBasicMaterial map={woodTexture}/>
+			</mesh>
+		</>
+	)
+}
+
+export function MuroY({longitud, altura}) {
+	function mapLengthToRange(l, rangeMin, rangeMax) {
+		const lengthMin = 0;
+		const lengthMax = 100;
+		const length = (l - lengthMin) / (lengthMax - lengthMin);
+		const range = (rangeMax - rangeMin) * length + rangeMin;
+		return range;
+	}
+
+	const numPilares = Math.floor((longitud/0.6)+1);
+	const porcentajeEspacio = 100/(longitud/0.6)
+	
+	const pilares = useMemo(() => {
+		const result = [];
+		let length = 0;
+        for (let i = 0; i < numPilares; i++) {
+			const range = mapLengthToRange(length, -0.5, 0.5);
+			length += porcentajeEspacio
+            result.push(<PilarY ubicacion={range} altura={altura} key={i}/>);
+        }
+        return result;
+    }, [longitud, altura, numPilares]);
+
+	return (
+		<>
+		{pilares}
+		</>
+	)
+}
+
+export function PilarY({ubicacion, altura}) {
+
+	return (
+		<>
+			<mesh
+			position={[ ubicacion, 0,0]}
+			scale={[0.01,1,1]}
 			>
 				<boxGeometry args={[1, 1, 1]}/>
 				<meshBasicMaterial map={woodTexture}/>
