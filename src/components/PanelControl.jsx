7393165180c2e1e2314719fb54cf5c26661eaacf
GoroGabox <alfons.diaz97@gmail.com>
2023-04-05 17:02:14 -0300

    actualizacion a panel de control

--- src/components/PanelControl.jsx
+++ src/components/PanelControl.jsx
@@ -60,7 +60,7 @@ function PanelControl() {
                     </div>
                 </div>
                 <div className="flex flex-col">
-                    <label htmlFor="z">Altura (metros):</label>
+                    <label htmlFor="z">Altura muros (metros):</label>
                     <div className="flex justify-between">
                         <input
                             className="flex w-3/4"
@@ -85,7 +85,75 @@ function PanelControl() {
                     </div>
                 </div>
             </div>
-            <div className="mt-5">Altura persona : 1.7 metros</div>
+            <hr className='bg-black mt-5'></hr>
+            <div className="mt-5 w-full">
+                <h2>Especificaciones: </h2>
+                <div className="flex flex-col mt-3 pl-3">
+                    <h2>Losa: </h2>
+                    <div className="grid grid-cols-2 justify-between">
+                        <label htmlFor="losa1">Vivienda (1/2/3):</label>
+                        <input
+                            className="flex w-fit mx-auto"
+                            type="radio"
+                            id="losa1"
+                            name='losa'
+                        />
+                        <label htmlFor="losa2" >Estaci. (1/2/2):</label>
+                        <input
+                            className="flex w-fit mx-auto"
+                            type="radio"
+                            id="losa2"
+                            name='losa'
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col mt-3 pl-3">
+                    <h2>Muros: </h2>
+                    <div className="grid grid-cols-2 justify-between">
+                        <label htmlFor="muro1" >Metalcom:</label>
+                        <input
+                            className="flex w-fit mx-auto"
+                            type="radio"
+                            id="muro1"
+                            name='muro'
+                        />
+                        <label htmlFor="muro2" >Madera:</label>
+                        <input
+                            className="flex w-fit mx-auto"
+                            type="radio"
+                            id="muro"
+                            name='muro'
+                        />
+                        <label htmlFor="muro3" >Ladrillo:</label>
+                        <input
+                            className="flex w-fit mx-auto"
+                            type="radio"
+                            id="muro3"
+                            name='muro'
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col mt-3 pl-3">
+                    <h2>Techo: </h2>
+                    <div className="grid grid-cols-2 justify-between">
+                        <label htmlFor="techo1" >1 Pendiente:</label>
+                        <input
+                            className="flex w-fit mx-auto"
+                            type="radio"
+                            id="techo1"
+                            name='techo'
+                        />
+                        <label htmlFor="losa2" >2 Pendiente:</label>
+                        <input
+                            className="flex w-fit mx-auto"
+                            type="radio"
+                            id="techo2"
+                            name='techo'
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="mt-5">** Altura persona : 1.7 metros **</div>
         </div>
     </div>
   )
