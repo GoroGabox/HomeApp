1651d8d773e646dc809cbda6d17808ca169e6ed6
GoroGabox <alfons.diaz97@gmail.com>
2023-04-06 02:53:54 -0300

    bug tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,22 +18,6 @@ const navLinks = [
   },
 ];
 
-const styles = {
-  paddingX: "sm:px-16 px-6",
-  paddingY: "sm:py-16 py-6",
-  padding: "sm:px-16 px-6 sm:py-16 py-10",
-
-  heroHeadText:
-    "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
-  heroSubText:
-    "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
-
-  sectionHeadText:
-    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
-  sectionSubText:
-    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
-};
-
 function Navbar() {
   
   let {user, logout} = useContext(AuthContext)
@@ -59,9 +43,7 @@ function Navbar() {
 
   return (
     <nav
-      className={`${
-        styles.paddingX
-      } w-full flex items-center py-5 fixed top-0 z-20 ${
+      className={`px-6 w-full flex items-center py-5 fixed top-0 z-20 ${
         scrolled ? "bg-black" : "bg-transparent"
       }`}
     >
